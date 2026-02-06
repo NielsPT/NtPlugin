a376becc7d3a9651052ea374d50c15ab8c68bdab
Niels Thøgersen <niels.thoegersen@gmail.com>
2026-02-06 02:03:53 +0100

    Added RmsSensor in milliseconds.

---
+++ lib/MsRmsSensor.h
@@ -0,0 +1,86 @@
+/*
+ * Copyright (C) 2026 Niels Thøgersen, NTlyd
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ **/
+
+#pragma once
+
+#include "Component.h"
+#include "Stereo.h"
+#include <algorithm>
+#include <array>
+
+namespace NtFx {
+constexpr int maxSampleDLineLen = 192 * 8;
+constexpr int maxMsDLineLen     = 1000;
+
+template <typename signal_t>
+struct MsRmsSensor : public Component<Stereo<signal_t>> {
+  bool resetAccums   = false;
+  int msDLineLen     = maxMsDLineLen;
+  int sampleDLineLen = maxSampleDLineLen;
+  std::array<Stereo<signal_t>, maxSampleDLineLen> samleDLine;
+  std::array<Stereo<signal_t>, maxMsDLineLen> msDLine;
+  Stereo<signal_t> sampleAccum;
+  Stereo<signal_t> msAccum;
+  int sampleIdx;
+  int msIdx;
+
+  virtual Stereo<signal_t> process(Stereo<signal_t> x) noexcept override {
+    if (this->resetAccums) {
+      this->sampleIdx   = 0;
+      this->msIdx       = 0;
+      this->sampleAccum = 0;
+      this->msAccum     = 0;
+      std::fill(this->samleDLine.begin(), this->samleDLine.end(), 0);
+      std::fill(this->msDLine.begin(), this->msDLine.end(), 0);
+      this->resetAccums = false;
+      return 0;
+    }
+
+    auto x2 = x * x;
+    if (x2 != x2) { x2 = signal_t(0.0); }
+    this->sampleAccum += x2 - this->samleDLine[this->sampleIdx];
+    this->samleDLine[this->sampleIdx] = x2;
+    if (++this->sampleIdx >= this->sampleDLineLen) {
+      this->sampleIdx = 0;
+      this->msAccum += sampleAccum - this->msDLine[this->msIdx];
+      this->msDLine[this->msIdx] = sampleAccum;
+      if (++this->msIdx >= this->msDLineLen) { this->msIdx = 0; }
+    }
+    return this->getRms();
+  }
+
+  virtual void update() noexcept override { }
+
+  virtual void reset(float fs) noexcept override {
+    this->sampleDLineLen = fs / 1000;
+    this->resetAccums    = true;
+    this->update();
+  }
+
+  Stereo<signal_t> getRms() const noexcept {
+    Stereo<signal_t> y = gcem::sqrt(signal_t(2.0) * this->msAccum
+        / signal_t(this->sampleDLineLen * this->msDLineLen));
+    if (y != y) { y = signal_t(0.0); }
+    return y;
+  }
+
+  void setRmsAvgTime(int t_ms) {
+    this->msDLineLen  = t_ms;
+    this->resetAccums = true;
+  }
+};
+}
--- plugins/ntRmsMeter.h
+++ plugins/ntRmsMeter.h
@@ -1,18 +1,41 @@
+/*
+ * Copyright (C) 2026 Niels Thøgersen, NTlyd
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ **/
+
 #pragma once
 
+#include "lib/MsRmsSensor.h"
+#include "lib/MultitapRmsSensor.h"
 #include "lib/Plugin.h"
-#include "lib/RmsSensor.h"
 #include "lib/Stereo.h"
 
 template <typename signal_t>
 struct ntRmsMeter : NtFx::NtPlugin<signal_t> {
-  enum Meter { Peak, RMS_10_ms, RMS_100_ms, RMS_1_s };
-  NtFx::RmsSensor<signal_t> sensor;
+  enum Meter { Peak, RMS, RMS_10_ms, RMS_100_ms, RMS_1_s };
+
+  NtFx::MultitapRmsSensor<signal_t> multitap;
+  NtFx::MsRmsSensor<signal_t> msSensor;
   signal_t decay_s = 0.1;
   signal_t hold_s  = 2;
+  signal_t tRms_ms = 10;
+
   ntRmsMeter() {
     this->uiSpec.meters = {
-      { "Peak", .hasScale = true, .minVal_db = -50 },
+      { "Peak", .minVal_db = -50 },
+      { "RMS", .hasScale = true, .minVal_db = -50 },
       { "RMS_10_ms", .minVal_db = -50 },
       { "RMS_100_ms", .minVal_db = -50 },
       { "RMS_1_s", .hasScale = true, .minVal_db = -50 },
@@ -20,27 +43,35 @@ struct ntRmsMeter : NtFx::NtPlugin<signal_t> {
     this->secondaryKnobs = {
       { &this->decay_s, "Decay", " s", 0, 1 },
       { &this->hold_s, "Hold", " s", 0, 10 },
+      { &this->tRms_ms, "RMS_Time", " ms", 1, 1000 },
     };
     this->uiSpec.meterHeight_dots = 25;
     this->updateDefaults();
   }
+
   virtual NtFx::Stereo<signal_t> process(
       NtFx::Stereo<signal_t> x) noexcept override {
-    sensor.process(x);
     this->template updatePeakLevel<Peak>(x);
-    this->template updatePeakLevel<RMS_10_ms>(sensor.getRms(1));
-    this->template updatePeakLevel<RMS_100_ms>(sensor.getRms(2));
-    this->template updatePeakLevel<RMS_1_s>(sensor.getRms(3));
+    msSensor.process(x);
+    this->template updatePeakLevel<RMS>(msSensor.getRms());
+    multitap.process(x);
+    this->template updatePeakLevel<RMS_10_ms>(multitap.getRms(1));
+    this->template updatePeakLevel<RMS_100_ms>(multitap.getRms(2));
+    this->template updatePeakLevel<RMS_1_s>(multitap.getRms(3));
     return x;
   }
+
   virtual void update() noexcept override {
     for (auto& m : this->uiSpec.meters) { m.decay_s = this->decay_s; }
     for (auto& m : this->uiSpec.meters) { m.hold_s = this->hold_s; }
+    this->msSensor.setRmsAvgTime(this->tRms_ms);
     this->uiNeedsUpdate = true;
   }
+
   virtual void reset(float fs) noexcept override {
     this->fs = fs;
-    this->sensor.reset(fs);
+    this->multitap.reset(fs);
+    this->msSensor.reset(fs);
     this->update();
   }
 };
