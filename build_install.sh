ca010cc201e6d6611b5a0a46e7974d793abb82a7
Niels Thøgersen <niels.thoegersen@gmail.com>
2026-04-13 23:07:43 +0200

    Integrated pluginval into build_install.sh

--- build_install.sh
+++ build_install.sh
@@ -18,8 +18,11 @@
 # You are free to download, build and use this code for commercial
 # purposes. Just don't resell it or a build of it, modified or otherwise.
 #
+
+#
 # @brief This script will build all plugins in the 'plugins' directory
-# in release mode and install them on the system if possible.
+# in release mode and install them on the system if possible. Add "-t" as arg to
+# run tests on lib and all plugins.
 #
 
 # Configuration
@@ -31,15 +34,16 @@ ID_FILE="$ARTIFACTS_DIR/plugin_ids.txt" # File to store plugin IDs
 TEST_SCRIPT_DIR="testWrapper"
 TEST_DIR="test"
 
-if [ ! -d .venv ]; then
-  python3 -m venv .venv;
-  . ./.venv/bin/activate;
-  pip install -r ${TEST_SCRIPT_DIR}/requirements.txt;
-else
-  . ./.venv/bin/activate;
+if [[ $# -gt 0 ]] && [[ "$1" == "-t" ]]; then
+  if [ ! -d .venv ]; then
+    python3 -m venv .venv;
+    . ./.venv/bin/activate;
+    pip install -r ${TEST_SCRIPT_DIR}/requirements.txt;
+  else
+    . ./.venv/bin/activate;
+  fi
+  python ./${TEST_SCRIPT_DIR}/test.py run all || return 1
 fi
-python ./${TEST_SCRIPT_DIR}/test.py run all || exit 1
-
 
 # Create artifacts directory if it doesn't exist
 mkdir -p "$ARTIFACTS_DIR"
@@ -65,10 +69,10 @@ while IFS= read -r header_file; do
     echo "Reusing existing plugin ID: $plugin_id"
   fi
 
-  # Step 1: Run cmake with the plugin name and captured output
+  # Run cmake with the plugin name and captured output
   echo "Running cmake for $plugin_name..."
   cmake_output=$(cmake -B "$BUILD_DIR" -S "$JUCE_WRAPPER_DIR" -DNTFX_PLUGIN="$plugin_name" \
-    ${plugin_id:+"-DNTFX_ID=$plugin_id"} 2>&1)
+    ${plugin_id:+"-DNTFX_ID=$plugin_id"} -DCMAKE_BUILD_TYPE=Release 2>&1)
   echo $cmake_output
 
   # Extract the plugin ID from cmake output if we didn't reuse one
@@ -87,15 +91,23 @@ while IFS= read -r header_file; do
     plugin_ids["$plugin_name"]="$plugin_id"
   fi
 
-  # Step 2: Build the project
+  # Build the project
   echo "Building $plugin_name..."
-  cmake --build "$BUILD_DIR" --config release -j$(nproc --all)
+  cmake --build "$BUILD_DIR" -j$(nproc --all)
 
-  # Step 3: Find the plugin-specific artifacts directory
+  if [[ $# -gt 0 ]] && [[ "$1" == "-t" ]]; then
+    # A short sleep to make sure the plugin exists before validation.
+    sleep 0.1
+  
+    # Test the plugin with pluginval
+    ctest --test-dir "$BUILD_DIR" || return 1
+  fi
+
+  # Find the plugin-specific artifacts directory
   plugin_artefacts_dir="$BUILD_DIR/${plugin_name}_artefacts"
 
   if [ -d "$plugin_artefacts_dir" ]; then
-    # Step 4: Copy the entire plugin artifacts directory to the final location
+    # Copy the entire plugin artifacts directory to the final location
     echo "Copying artifacts for $plugin_name..."
     cp -r "$plugin_artefacts_dir" "$ARTIFACTS_DIR/"
 
